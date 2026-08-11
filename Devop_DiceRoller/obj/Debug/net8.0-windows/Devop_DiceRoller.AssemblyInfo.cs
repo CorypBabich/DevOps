b62eb0a9a8c8727e13340d13a5d9262ce06457d9
CorypBabich <cpbabich@email.NEIT.edu>
2026-08-11 17:03:03 -0400

    Mock data
    Added demonstration picture to files of the app using mock data to simulate data being pulled from SQL table.

--- Devop_DiceRoller/obj/Debug/net8.0-windows/Devop_DiceRoller.AssemblyInfo.cs
+++ Devop_DiceRoller/obj/Debug/net8.0-windows/Devop_DiceRoller.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Devop_DiceRoller")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4ffcb510149fbd23d6221ca21cd54360f96ea447")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+33fd70b2b8beb14e10946a5084ab63e1480b85e8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Devop_DiceRoller")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Devop_DiceRoller")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
